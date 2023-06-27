6677a1395b3086beaeb34ca0e2e0338830159f7c
Sen Tian <sen@tiansen.me>
2023-06-27 20:24:25 +0800

    init & add linkedin profile

---
+++ tools/tools.py
@@ -0,0 +1,10 @@
+from langchain.serpapi import SerpAPIWrapper
+
+
+def get_profile_url(text: str) -> str:
+    """Searches for Linkedin profile page."""
+    search = SerpAPIWrapper()
+
+    res = search.run(text)
+
+    return res
